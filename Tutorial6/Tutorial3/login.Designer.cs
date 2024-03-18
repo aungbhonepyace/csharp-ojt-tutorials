6ca65934ea19a3abaad4f226cc1092b409cfdc27
Joistich <aungbonepyace@gmail.com>
2024-03-19 03:28:44 +0630

    update

--- Tutorial6/Tutorial3/login.Designer.cs
+++ Tutorial6/Tutorial3/login.Designer.cs
@@ -76,6 +76,7 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
             this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // login
             // 
